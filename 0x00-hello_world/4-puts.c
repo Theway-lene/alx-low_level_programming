c00fce18f90651320b1124aa08dce50ec9859194
Theway-lene <mistalene1@gmail.com>
2022-09-13 06:00:49 +0000

    Some C file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * myputsass - this is the main function
+ * this function prints out the string passed into it
+*/
+void myputsass(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+
+}
