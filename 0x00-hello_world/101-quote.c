131060724a050ee160baa4c6f338f5b30743289b
Theway-lene <mistalene1@gmail.com>
2022-09-14 02:15:05 +0000

    backslash to show the quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main function is to print exactly what's passed into it
+ * Return: is 1, let's see how it works
+ */
+
+int main()
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
