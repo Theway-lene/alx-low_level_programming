ea191cbb09f2c15171e62dc4a99491b1d9967239
Theway-lene <mistalene1@gmail.com>
2022-09-14 01:29:47 +0000

    printf usage

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function to print text with a new line
+ * nothing else, just pass out values
+ * Return: value passed
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
