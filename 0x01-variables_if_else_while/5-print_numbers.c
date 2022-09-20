cadf9c594046f1cdd87abaeac42f390a161c7d5d
Theway-lene <mistalene1@gmail.com>
2022-09-20 17:44:16 +0000

    0 - 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* main function prints out numbers in base 10
+ * Return: void integer
+*/
+
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		printf("%d \n", a);
+		a++;
+	}
+	
+	return (0);
+}
