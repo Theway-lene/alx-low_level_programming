28a9c4034ff21c505529ec55c67dd112aa162437
Theway-lene <mistalene1@gmail.com>
2022-09-20 17:37:55 +0000

    exceptions

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - function prints alphabets with exceptions
+ * Return: brings a void integer
+*/
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a != 'e' && a != 'q')
+			putchar (a);
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
