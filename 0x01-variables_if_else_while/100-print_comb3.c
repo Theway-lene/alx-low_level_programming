bbe56ec46db451c21450480abf1d88288a3e7ee1
Theway-lene <mistalene1@gmail.com>
2022-09-21 11:51:09 +0000

    comma at the end too

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - function print combination of double digit numbers
+ * Return: void to end function
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 48; a < 57; a++)
+	{
+		for (b = 49; b < 58; b++)
+		{
+			if (b > a)
+			{	putchar(a);
+				putchar(b);
+				putchar(',');
+				putchar(' ');
+			}
+			else if (b <= a)
+			{};
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
