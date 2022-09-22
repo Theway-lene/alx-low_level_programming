737683743d2a1fa3e0f0e3caf6bd55dc5badfd06
Theway-lene <mistalene1@gmail.com>
2022-09-22 14:13:43 +0000

    i pushed all at once

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - show if n is greater, less or = 0
+ * @n: interger argument to be passed
+ * Return: 1 if true else -1 else 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
